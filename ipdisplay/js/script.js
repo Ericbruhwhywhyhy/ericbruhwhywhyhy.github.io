26e200ad23d4734f0f9a823cd65e1236ebe9e001
Ericbruhwhywhyhy <160207601+Ericbruhwhywhyhy@users.noreply.github.com>
2024-04-19 20:58:25 +0700

    Update script.js

--- ipdisplay/js/script.js
+++ ipdisplay/js/script.js
@@ -1,10 +1,9 @@
+"use strict";
 const doSomething = getIP();
-
-async function getIP(): Promise<string> {
-    
+async function getIP() {
     const element_ip = document.getElementById("vd_ip") || new HTMLElement();
     const promise = await fetch("//ipinfo.io/json");
     const data = await promise.json();
-    element_ip.innerHTML = data.ip + " | " + data.hostname +  " | " + data.city + " | " + data.country ;
+    element_ip.innerHTML = data.ip + " | " + data.hostname + " | " + data.city + " | " + data.country;
     return data;
 }
