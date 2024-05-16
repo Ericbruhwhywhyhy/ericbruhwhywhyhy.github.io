ddc514d012db91c0d2a457d8a6a29e14795008ea
Ericbruhwhywhyhy <ericdo0169@gmail.com>
2024-05-16 08:03:36 +0700

    update

---
+++ js/scriptscript.js
@@ -0,0 +1,6 @@
+function tetriohover(){
+  document.getElementById("tetrio").src="./assets/tetrio-color.svg"
+}
+function tetrionohover() {
+  document.getElementById("tetrio").src = "./assets/tetrio-mono.svg"
+}
