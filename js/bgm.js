c21e4bee32ca5fd1a9e50b387dfd6c0390ef1ba5
Ericbruhwhywhyhy <ericdo0169@gmail.com>
2024-06-01 17:56:05 +0700

    update

---
+++ js/bgm.js
@@ -0,0 +1,8 @@
+var bgm = new Howl({
+  src: ['https://ericbruhwhywhyhy.guthub.io/lobbytime.mp3'],
+  preload: true,
+  loop: true
+});
+console.log("its play!")
+
+
--- js/blogjs/bgm.js
+++
@@ -1,7 +0,0 @@
-var sound = new Howl({
-  src: ['https://ericbruhwhywhyhy.github.io/awebsite/3008friday.mp3'],
-  preload: true,
-  loop: true
-});
-
-sound.play();
