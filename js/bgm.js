0892dd7be0e7a89e7e6f99d742bc1098e008a05c
Ericbruhwhywhyhy <ericdo0169@gmail.com>
2024-06-02 14:29:06 +0700

    update

--- js/bgm.js
+++ js/bgm.js
@@ -1,8 +1,12 @@
 var bgm = new Howl({
-  src: ['https://ericbrericbruhwhywhyhy.github.io/awebsite/lobbytime.mp3'],
+  src: ['https://ericbruhwhywhyhy.github.io/awebsite/lobbytime.mp3'],
   preload: true,
   loop: true
 });
 console.log("its play!")
-
-
+bgm.play()
+twemoji.parse(document.body, {
+  folder: 'svg',
+  size: "16x16",
+  ext: '.svg'
+})
